3e4fd3c8d30abafe2e26e1350c635f1ff3cb2eea
Shane <shaneedelstein@gmail.com>
2026-02-12 11:47:28 -0500

    Add endpoint headers and refactor user handling functions

---
+++ src/server/endpoints/Symptoms.cpp
@@ -0,0 +1,12 @@
+#include "Symptoms.h"
+
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <charconv>
+
+using json = nlohmann::json;
