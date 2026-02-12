de9d0db44f050744ee7407f632e20d31ab4bfeb3
Kalan (Kay) Roye <kroye@uwo.ca>
2026-02-12 12:45:31 -0500

    Implement GET user by ID endpoint in UsersHandler

--- src/server/endpoints/Users.cpp
+++ src/server/endpoints/Users.cpp
@@ -17,7 +17,30 @@ UsersHandler::handle(const http::request<http::string_body> &req,
                      DatabaseManager &db,
                      net::thread_pool &pool)
 {
-    // TODO: Implement user CRUD and login logic
+    // GET /users/{id}
+    if (req.method() == http::verb::get && path_parts.size() == 2)
+    {
+        int userId = http_utils::parse_int(path_parts[1]);
+        if (userId < 0)
+        {
+            co_return http_utils::make_error(http::status::bad_request,
+                                            "Invalid user ID", req.version(), req.keep_alive());
+        }
+
+        json result = db.getUserById(userId);
+        
+        // Check if the result contains an error
+        if (result.contains("error"))
+        {
+            co_return http_utils::make_error(http::status::not_found,
+                                            result["error"], req.version(), req.keep_alive());
+        }
+
+        co_return http_utils::make_json_response(http::status::ok,
+                                                result, req.version(), req.keep_alive());
+    }
+
+    // TODO: Implement other user endpoints
     co_return http_utils::make_error(http::status::not_implemented,
                                      "Not implemented", req.version(), req.keep_alive());
 }
